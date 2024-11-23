528162651a1ff4efde46c14b159760f94b5dbf4f
adeliafitri <adeliafitri972@gmail.com>
2024-11-23 20:39:49 +0700

    [fix] error handling import excel

--- app/Imports/MahasiswaImportExcel.php
+++ app/Imports/MahasiswaImportExcel.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\User;
 use App\Models\Dosen;
 use App\Models\Mahasiswa;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -13,24 +14,34 @@ class MahasiswaImportExcel implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        $password = $row['nim'];
-        $auth =  User::create([
-            'username' => $row['nim'],
-            'password' => Hash::make($password),
-            'role' => 'mahasiswa'
-        ]);
+        try {
+            $password = $row['nim'];
+            $auth =  User::create([
+                'username' => $row['nim'],
+                'password' => Hash::make($password),
+                'role' => 'mahasiswa'
+            ]);
 
-        $id_auth = $auth->id;
+            $id_auth = $auth->id;
 
-        // $defaultStatus = 'aktif';
+            // $defaultStatus = 'aktif';
 
-        Mahasiswa::create([
-            'id_auth' => $id_auth,
-            'nama' => $row['nama_mahasiswa'],
-            'nim' => $row['nim'],
-            'telp' => $row['no_telp'],
-            'angkatan' => $row['angkatan'],
-            'tahun_lulus' => '0000'
-        ]);
+            Mahasiswa::create([
+                'id_auth' => $id_auth,
+                'nama' => $row['nama_mahasiswa'],
+                'nim' => $row['nim'],
+                'telp' => $row['no_telp'],
+                'angkatan' => $row['angkatan'],
+                'tahun_lulus' => '0000'
+            ]);
+        } catch (\Exception $e) {
+            // Logging error ke dalam laravel log file
+            Log::error("Error importing kelas: " . $e->getMessage(), [
+                'row' => $row
+            ]);
+
+            // throw new \Exception("Error di baris: " . json_encode($row) . ". Pesan: " . $e->getMessage());
+            throw new \Exception($e->getMessage());
+        }
     }
 }
